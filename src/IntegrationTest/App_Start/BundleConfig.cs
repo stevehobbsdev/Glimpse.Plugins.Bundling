d4f5c26d2d49118111b09c11e72937ba3d9e633f
Steve Hobbs <elkdanger@googlemail.com>
2013-07-25 14:04:42 +0100

    Added a basic web project for integration testing the plugin

---
+++ src/IntegrationTest/App_Start/BundleConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Optimization;
+
+namespace IntegrationTest
+{
+    public static class BundleConfig
+    {
+        public static void Configure(BundleCollection bundles)
+        {
+            // Used on the site
+            bundles.Add(new StyleBundle("~/site/css")
+                .Include("~/content/site.css"));
+
+            // Unused bundle - test that the plugin can distinguish
+            bundles.Add(new ScriptBundle("~/jquery")
+                .Include("~/scripts/jquery-{version}.js"));
+        }
+    }
+}
---
+++ src/IntegrationTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IntegrationTest.MvcApplication" Language="C#" %>
