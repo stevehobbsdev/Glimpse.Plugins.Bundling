cddba75bfe06edca94250659ab8078ceff904065
Steve Hobbs <elkdanger@googlemail.com>
2013-11-15 14:35:40 +0000

    File rename

---
+++ src/Glimpse.Plugins.Bundling/GlimpseBundlePluginHttpModule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Web;
+using System.Web.Optimization;
+using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+
+namespace Glimpse.Plugins.Bundling
+{
+    public class GlimpseBundlePluginHttpModule : IHttpModule
+    {
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += OnBeginRequest;
+        }
+
+        void OnBeginRequest(object sender, EventArgs e)
+        {
+            if (!(BundleResolver.Current is GlimpsePluginBundleResolver))
+            {
+                BundleResolver.Current = new GlimpsePluginBundleResolver(BundleResolver.Current);
+            }
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
